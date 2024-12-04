8828df5093a18235894d5c1dd5db9026b1f39f51
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-05 01:55:35 +0500

    code is added

--- script.js
+++ script.js
@@ -22,11 +22,24 @@ boxes.forEach((box) => {
     box.addEventListener("click",  function(){
         console.log("box was clicked");
         if(turnO) {
-            boxes.innerHTML = "O";
+            box.innerHTML = "O";
             turnO = false;
     }   else {
-        boxes.innerHTML = "X";
+        box.innerHTML = "X";
             turnO = true;
     }
-})
-})
+        box.disabled = true;
+        checkWin();
+    });
+});
+
+const checkWin = function(){
+    for(let pattern of winPatterns){
+       console.log(pattern[0], pattern[1], pattern[2]);
+       console.log(
+        boxes[pattern[0]].innerHTML,
+        boxes[pattern[1]].innerHTML,
+        boxes[pattern[2]].innerHTML
+    );     
+ };
+}
