19f322cceeae697d2da015432772f0292b7b0208
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-05 01:10:19 +0500

    code is added

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 // Target DOM elements:
-
 let boxes = document.querySelectorAll('.box');
 let resetBtn = document.querySelector("#reset-btn");
+
+let turnO = true;           //playerX, playerY
+
+// Store pattern in an array, wesy tu hum array ko string ki form mai b store krskty thy but its better way k aghr hum inko 
+// 2D array mai store krdain tuo...
+
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+]
+
